3e932d5b857aa0e3305ddeca07f61b81adf7b078
Anthony Diaz <antony.diaz@me.com>
2019-03-07 21:10:36 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgxsModule } from '@ngxs/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ScoreBarComponent } from './components/score-bar/score-bar.component';
+import { PresentationComponent } from './views/presentation/presentation.component';
+import { GameComponent } from './views/game/game.component';
+import { GameState } from './state/game.state';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, PresentationComponent, ScoreBarComponent, GameComponent],
+  imports: [BrowserModule, AppRoutingModule, NgxsModule.forRoot([GameState])],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/score-bar/score-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-score-bar',
+  templateUrl: './score-bar.component.html',
+  styleUrls: ['./score-bar.component.scss']
+})
+export class ScoreBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
